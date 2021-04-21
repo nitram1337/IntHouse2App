a259892aadfcb73f177e9f217744a99e47a3f916
Martin <mart605h@elevcampus.dk>
2021-04-21 21:58:07 +0200

    Added start to Graph and added the page and viewmodel for it.

---
+++ IntHouse2App/IntHouse2App/ViewModels/GraphViewModel.cs
@@ -0,0 +1,42 @@
+﻿using Microcharts;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entry = Microcharts.ChartEntry;
+
+namespace IntHouse2App.ViewModels
+{
+    public class GraphViewModel : BaseViewModel
+    {
+        public Chart TestChart { get; set; }
+
+        public GraphViewModel()
+        {
+            TestChart = new LineChart
+            {
+                Entries = new List<Entry>
+            {
+                new Entry(200)
+                {
+                    Color = SKColor.Parse("#FF1493"),
+                    Label = "January",
+                    ValueLabel = "200"
+                },
+                new Entry(400)
+                {
+                    Color = SKColor.Parse("#00BFFF"),
+                    Label = "February",
+                    ValueLabel = "400"
+                },
+                new Entry(-100)
+                {
+                    Color = SKColor.Parse("#00CED1"),
+                    Label = "March",
+                    ValueLabel = "200"
+                }
+            }
+            };
+        }
+    }
+}
---
+++ IntHouse2App/IntHouse2App/Views/GraphsPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using IntHouse2App.ViewModels;
+using Microcharts;
+using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Entry = Microcharts.ChartEntry;
+
+namespace IntHouse2App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GraphsPage : ContentPage
+    {
+        //List<Entry> entries = new List<Entry>
+        //{
+        //    new Entry(200)
+        //    {
+        //        Color = SKColor.Parse("#FF1493"),
+        //        Label = "January",
+        //        ValueLabel = "200"
+        //    },
+        //    new Entry(400)
+        //    {
+        //        Color = SKColor.Parse("#00BFFF"),
+        //        Label = "February",
+        //        ValueLabel = "400"
+        //    },
+        //    new Entry(-100)
+        //    {
+        //        Color = SKColor.Parse("#00CED1"),
+        //        Label = "March",
+        //        ValueLabel = "200"
+        //    }
+        //};
+        GraphViewModel _viewModel;
+
+        public GraphsPage()
+        {
+            InitializeComponent();
+            //Chart1.Chart = new RadialGaugeChart { Entries = new List<Entry>
+            //    {
+            //        new Entry(200)
+            //        {
+            //            Color = SKColor.Parse("#FF1493"),
+            //            Label = "January",
+            //            ValueLabel = "200"
+            //        },
+            //        new Entry(400)
+            //        {
+            //            Color = SKColor.Parse("#00BFFF"),
+            //            Label = "February",
+            //            ValueLabel = "400"
+            //        },
+            //        new Entry(-100)
+            //        {
+            //            Color = SKColor.Parse("#00CED1"),
+            //            Label = "March",
+            //            ValueLabel = "200"
+            //        }
+            //    }
+            //};
+            BindingContext = _viewModel = new GraphViewModel();
+        }
+
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    _viewModel.OnAppearing();
+        //}
+    }
+}
