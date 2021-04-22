90638822758c58c19926b7a813a46f3d1c1e5fb2
Martin <mart605h@elevcampus.dk>
2021-04-22 15:39:48 +0200

    Added behavior to change textcolor on selectedindexchanged

---
+++ IntHouse2App/IntHouse2App/Behaviors/TimeFrameBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace IntHouse2App.Behaviors
+{
+    public class TimeFrameBehavior : Behavior<Picker>
+    {
+        protected override void OnAttachedTo(Picker picker)
+        {
+            picker.SelectedIndexChanged += OnSelectedIndexChanged;
+            base.OnAttachedTo(picker);
+        }
+
+        protected override void OnDetachingFrom(Picker picker)
+        {
+            picker.SelectedIndexChanged -= OnSelectedIndexChanged;
+            base.OnDetachingFrom(picker);
+        }
+
+        void OnSelectedIndexChanged(object sender, EventArgs args)
+        {
+            switch (((Picker)sender).SelectedIndex)
+            {
+                case 0:
+                    ((Picker)sender).TextColor = Color.Purple;
+                    break;
+                case 1:
+                    ((Picker)sender).TextColor = Color.Blue;
+                    break;
+                case 2:
+                    ((Picker)sender).TextColor = Color.Green;
+                    break;
+                default:
+                    ((Picker)sender).TextColor = Color.Red;
+                    break;
+            }
+        }
+    }
+}
