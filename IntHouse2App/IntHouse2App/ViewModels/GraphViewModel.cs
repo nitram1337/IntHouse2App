76f51ec78a957101015976e1c4ee08b6861e84d3
Martin <mart605h@elevcampus.dk>
2021-04-22 08:04:22 +0200

    Fixed graph

--- IntHouse2App/IntHouse2App/ViewModels/GraphViewModel.cs
+++ IntHouse2App/IntHouse2App/ViewModels/GraphViewModel.cs
@@ -1,42 +1,91 @@
-﻿using Microcharts;
+﻿using IntHouse2App.Models;
+using Microcharts;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
 using Entry = Microcharts.ChartEntry;
 
 namespace IntHouse2App.ViewModels
 {
     public class GraphViewModel : BaseViewModel
     {
-        public Chart TestChart { get; set; }
+        //public ObservableCollection<Measurement> Measurements;
+        public List<Entry> Entries;
+        private Chart _chartMeasurements;
+
+        public Chart ChartMeasurements
+        {
+            get => _chartMeasurements;
+            set => SetProperty(ref _chartMeasurements, value);
+        }
+
+        public Command LoadChartMeasurementsCommand { get; }
 
         public GraphViewModel()
         {
-            TestChart = new LineChart
-            {
-                Entries = new List<Entry>
-            {
-                new Entry(200)
-                {
-                    Color = SKColor.Parse("#FF1493"),
-                    Label = "January",
-                    ValueLabel = "200"
-                },
-                new Entry(400)
-                {
-                    Color = SKColor.Parse("#00BFFF"),
-                    Label = "February",
-                    ValueLabel = "400"
-                },
-                new Entry(-100)
-                {
-                    Color = SKColor.Parse("#00CED1"),
-                    Label = "March",
-                    ValueLabel = "200"
-                }
-            }
-            };
+            Title = "Graph Measurements";
+            //Measurements = new ObservableCollection<Measurement>();
+            Entries = new List<Entry>();
+            LoadChartMeasurementsCommand = new Command(async () => await ExecuteLoadAllChartMeasurementsCommand());
+
+            IsConnected = Connectivity.NetworkAccess != NetworkAccess.Internet;
+            Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
         }
+
+        async Task ExecuteLoadAllChartMeasurementsCommand()
+        {
+            IsBusy = true;
+            try
+            {
+                //Measurements.Clear();
+                Entries.Clear();
+                var measurements = await _measurementsService.GetMeasurementsTimeFilteredAsync(TimeFrame.LatestWeek);
+                foreach (Measurement measurement in measurements)
+                {
+                    //Measurements.Add(measurement);
+                    Entries.Add(new Entry(measurement.Temperature)
+                    {
+                        Color = SKColor.Parse("#038cfc"),
+                        Label = measurement.TimeCreated.ToString(),
+                        TextColor = SKColor.Parse("#000000"),
+                        ValueLabel = measurement.Temperature.ToString()
+                    });
+                }
+
+                ChartMeasurements = new LineChart { Entries = Entries };
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+                throw;
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private void Connectivity_ConnectivityChanged(object sender, ConnectivityChangedEventArgs e)
+        {
+            IsConnected = e.NetworkAccess != NetworkAccess.Internet;
+        }
+
+        public void OnAppearing()
+        {
+            IsBusy = true;
+        }
+
+
+        public void Dispose()
+        {
+            Connectivity.ConnectivityChanged -= Connectivity_ConnectivityChanged;
+        }
+
     }
 }
--- IntHouse2App/IntHouse2App/Views/GraphsPage.xaml.cs
+++ IntHouse2App/IntHouse2App/Views/GraphsPage.xaml.cs
@@ -16,61 +16,19 @@ namespace IntHouse2App.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class GraphsPage : ContentPage
     {
-        //List<Entry> entries = new List<Entry>
-        //{
-        //    new Entry(200)
-        //    {
-        //        Color = SKColor.Parse("#FF1493"),
-        //        Label = "January",
-        //        ValueLabel = "200"
-        //    },
-        //    new Entry(400)
-        //    {
-        //        Color = SKColor.Parse("#00BFFF"),
-        //        Label = "February",
-        //        ValueLabel = "400"
-        //    },
-        //    new Entry(-100)
-        //    {
-        //        Color = SKColor.Parse("#00CED1"),
-        //        Label = "March",
-        //        ValueLabel = "200"
-        //    }
-        //};
         GraphViewModel _viewModel;
 
         public GraphsPage()
         {
             InitializeComponent();
-            //Chart1.Chart = new RadialGaugeChart { Entries = new List<Entry>
-            //    {
-            //        new Entry(200)
-            //        {
-            //            Color = SKColor.Parse("#FF1493"),
-            //            Label = "January",
-            //            ValueLabel = "200"
-            //        },
-            //        new Entry(400)
-            //        {
-            //            Color = SKColor.Parse("#00BFFF"),
-            //            Label = "February",
-            //            ValueLabel = "400"
-            //        },
-            //        new Entry(-100)
-            //        {
-            //            Color = SKColor.Parse("#00CED1"),
-            //            Label = "March",
-            //            ValueLabel = "200"
-            //        }
-            //    }
-            //};
+
             BindingContext = _viewModel = new GraphViewModel();
         }
 
-        //protected override void OnAppearing()
-        //{
-        //    base.OnAppearing();
-        //    _viewModel.OnAppearing();
-        //}
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
     }
 }
